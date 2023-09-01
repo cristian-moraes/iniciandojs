68edc1bcbe8936f8f44fd4d05891ad45b5ce802f
cristian-moraes <143741284+cristian-moraes@users.noreply.github.com>
2023-09-01 14:18:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const nome = prompt("qual seu nome?")
+const idade = prompt("qual a sua idade?")
+console.log (nome, idade);
+// string tipo texto por que foi escrito por extenso
+const resposta = prompt("Ola Cristian, voce tem 35 anos")
+console.log (resposta);
